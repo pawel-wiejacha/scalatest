d57fd94cfc89fd1abfafe26b5f46fb615d3b6f7d
Paweł Wiejacha <pawel.wiejacha@gmail.com>
2015-02-06 20:40:12 +0100

    Using release version that is compatbile with publishLocal

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -12,7 +12,7 @@ object ScalatestBuild extends Build {
   // > ++ 2.10.4
   val buildScalaVersion = "2.11.2"
 
-  val releaseVersion = "2.3.0-SNAP1"
+  val releaseVersion = "2.3.0-SNAPSHOT"
   val githubTag = "release-2.3.0-SNAP1-for-scala-2.11-and-2.10" // for scaladoc source urls
 
   val docSourceUrl =
